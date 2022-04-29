6d1ff0c628e7292f4e67f686b60b1c9cae8bf353
jmol <jozinho@centrum.sk>
2022-04-29 15:48:45 +0200

    Skuska v eclipse- nova trieda vytvorena

---
+++ src/com/learning/a_notes/Skusam.java
@@ -0,0 +1,16 @@
+package com.learning.a_notes;
+
+public class Skusam {
+
+	/**kratky popis o funkcii 
+	 */
+	public static void main(String[] args) {
+		// poznamka jednoriadkova
+		/*
+		 *viacriadkov poznamka 
+		 */
+		
+
+	}
+
+}
