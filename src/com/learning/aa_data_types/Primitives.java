c9237a196812870653e1cfcb42f1f505e5722e61
jmol <jozinho@centrum.sk>
2022-05-03 16:14:33 +0200

    Added new notes too primitives.java

---
+++ src/com/learning/aa_data_types/Primitives.java
@@ -0,0 +1,44 @@
+package com.learning.aa_data_types;
+
+class Primitives {
+	public static void main(String[] args) {
+		/*
+		 * primitivne datove typy
+		 * 8 primitivnych datovych typov,ktore sa delia na 4 skupiny
+		 * 1sk. cele cisla
+		 * 2sk. desatinne cisla
+		 * 3sk. znaky
+		 * 4sk. logicke hodnoty
+		 * 
+		 * 1sk. 
+		 * byte, short, int, long
+		 * 2sk.
+		 * float, double
+		 * 3sk.  
+		 * char (akykolvek 1 znak z ascii tabulky)
+		 * 4sk.
+		 * boolean (true false)
+		 *
+		 * staci zo zaciatku dobre vediet pouzivat int, double, char a boolean
+		 * 
+		 */
+		int vahaMukyVGramoch = 0;
+		System.out.println("Pocet gramov muky: " + vahaMukyVGramoch);
+		
+		int PocetLicencii;
+		char Otaznik;
+		int PocetRokov;
+		boolean retardovany;
+		int StupenRetardosti;
+		double VahaVKilogramoch;
+		boolean SchvalenieDokumentu;
+		double FrekvenciaCPU;
+		char ZalohovanieFlase;
+		boolean bezOS;
+		char pocujeNepocujeIrca;
+		boolean pocujeNepocujeIrca2;
+		int volumeSpeakers;
+		boolean powerONOFF,
+		
+	}
+}
